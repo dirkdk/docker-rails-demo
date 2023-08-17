89c5d549a1775458ad97b672e2d9c86d0ffaecb2
dirkdk <311562+dirkdk@users.noreply.github.com>
2023-08-16 18:03:01 -0700

    also node options for assets precompile

--- scripts/potential_asset_precompile.sh
+++ scripts/potential_asset_precompile.sh
@@ -2,7 +2,7 @@
 
 if [ $# -eq 0 ] || [ $1 != "not" ] ; then
   echo 'precompiling assets'
-  RAILS_ENV=production rake assets:precompile
+  RAILS_ENV=production rake assets:precompile NODE_OPTIONS=--openssl-legacy-provider
 else
   echo "argument is not, so not running precompile"
 fi
