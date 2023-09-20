635c431010eb34c09cc9962af06d04f76d948ad0
dirkdk <311562+dirkdk@users.noreply.github.com>
2023-09-19 19:08:19 -0700

    updated rails gems, fixed database warning

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module DockerRailsDemo
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_record.legacy_connection_handling = false
   end
 end
